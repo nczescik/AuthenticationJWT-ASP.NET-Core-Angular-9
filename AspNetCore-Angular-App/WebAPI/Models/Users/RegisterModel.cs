978771d5eac14a61689081731a7f91ed1e835a8a
norbert czescik <norbert.czescik@axes.ai>
2020-10-28 01:24:38 +0100

    Deleted unnecessary lines of code

--- AspNetCore-Angular-App/WebAPI/Models/Users/RegisterModel.cs
+++ AspNetCore-Angular-App/WebAPI/Models/Users/RegisterModel.cs
@@ -5,12 +5,6 @@ namespace WebAPI.Models.Users
     public class RegisterModel
     {
         [Required]
-        public string FirstName { get; set; }
-
-        [Required]
-        public string LastName { get; set; }
-
-        [Required]
         public string Username { get; set; }
 
         [Required]
